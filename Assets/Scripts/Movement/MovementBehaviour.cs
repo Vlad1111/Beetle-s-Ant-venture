5777683c47e798976d5a109251dcc48f84aa15a6
SuicidalBlob <sbengheci.vlad@yahoo.com>
2024-08-17 18:15:30 +0300

    change terain generatior and implement shader for grass

--- Assets/Scripts/Movement/MovementBehaviour.cs
+++ Assets/Scripts/Movement/MovementBehaviour.cs
@@ -7,6 +7,7 @@ public class MovementBehaviour : MonoBehaviour
     public Rigidbody rb;
     public float maxSpeed;
     public float baseSpeed;
+    public float breackPower;
     public float underwaterSpeed;
     public float rotationSpeed;
 
@@ -31,9 +32,14 @@ public class MovementBehaviour : MonoBehaviour
     {
         float speed = baseSpeed;
         var horizontalSpeed = new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude;
- 
-        rb.velocity += transform.forward * movement.x * speed * (maxSpeed - horizontalSpeed) * Time.deltaTime;
+
+        if (Mathf.Abs(movement.x) > 0)
+            rb.velocity += transform.forward * movement.x * speed * (maxSpeed - horizontalSpeed) * Time.deltaTime;
+        else rb.velocity -= new Vector3(rb.velocity.x, 0, rb.velocity.z) * breackPower * Time.deltaTime;
         transform.localEulerAngles += new Vector3(0, rotationSpeed * movement.y * Time.deltaTime, 0);
+
+        if (rb.velocity.y < 0)
+            rb.velocity += new Vector3(0, rb.velocity.y, 0) * Time.deltaTime;
 
         Vector3 desiredBodyAngles = Vector3.zero;
         if(bodyTerrainCollider.raycasHit != null)
@@ -43,7 +49,7 @@ public class MovementBehaviour : MonoBehaviour
             var newAngles = new Vector3(Mathf.Asin(pointN.z), 0, -Mathf.Asin(pointN.x));
             desiredBodyAngles = newAngles * Mathf.Rad2Deg;
         }
-        bodyMeshesParent.localRotation = Quaternion.Lerp(bodyMeshesParent.localRotation, Quaternion.Euler(desiredBodyAngles), 2 * Time.deltaTime);
+        bodyMeshesParent.localRotation = Quaternion.Lerp(bodyMeshesParent.localRotation, Quaternion.Euler(desiredBodyAngles), 5 * Time.deltaTime);
         //bodyMeshesParent.Rotate(desiredAxes, transform.localEulerAngles.y);
     }
 }
